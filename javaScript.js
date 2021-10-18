f942965e2889ea7fee822a6c2e8f29fb75f070ae
nmunro92 <n.munro92@gmail.com>
2021-10-18 21:30:34 +1100

    fixed rainbow button. add some more style. add size buttons, rather than user input.

--- javaScript.js
+++ javaScript.js
@@ -1,13 +1,11 @@
 const gameContainer = document.getElementById("gameContainer");
 
-
-//build a grid of users size
-function getSquares (){
+function onLoad () {
+    clearGrid ();
     gameContainer.innerHTML = "";
-    let n = parseInt(document.getElementById("squares").value);
 
-    for (let i = 0; i < n; i++) {
-            for (let j = 0; j < n; j++){
+    for (let i = 0; i < 20; i++) {
+            for (let j = 0; j < 20; j++){
             gameContainer.innerHTML+= '<div class="div2"> </div>';
             }
         
@@ -15,13 +13,63 @@ function getSquares (){
 
     //make sure it's a grid
     gameContainer.style.display = "inline-grid";
-    gameContainer.style.gridTemplateColumns =`repeat(${n}, auto)`;
+    gameContainer.style.gridTemplateColumns ='repeat(20, auto)';
 
     //add listens for when they're hovered over
     document.querySelectorAll(".div2").forEach(link => link.addEventListener('mouseover', (event) => {
         event.target.style.backgroundColor = "black";
   }))
 }
+
+onLoad ();
+blackSquares ();x
+
+
+//build a grid of users size
+function smallGrid (){
+    clearGrid ();
+    gameContainer.innerHTML = "";
+    for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++){
+            gameContainer.innerHTML+= '<div class="div2"> </div>';
+            }
+    }
+        
+    
+
+    //make sure it's a grid
+    gameContainer.style.display = "inline-grid";
+    gameContainer.style.gridTemplateColumns ="repeat(10, auto)";
+
+    //add listens for when they're hovered over
+    document.querySelectorAll(".div2").forEach(link => link.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = "black";
+  }))
+}
+
+
+function largeGrid (){
+    clearGrid ();
+    gameContainer.innerHTML = "";
+
+    for (let i = 0; i < 30; i++) {
+            for (let j = 0; j < 30; j++){
+            gameContainer.innerHTML+= '<div class="div2"> </div>';
+            }
+        }
+        
+    
+
+    //make sure it's a grid
+    gameContainer.style.display = "inline-grid";
+    gameContainer.style.gridTemplateColumns ="repeat(30, auto)";
+
+    //add listens for when they're hovered over
+    document.querySelectorAll(".div2").forEach(link => link.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = "black";
+  }))
+}
+
 
 // button to clear the grid
 function clearGrid (){
@@ -30,5 +78,28 @@ function clearGrid (){
         myArr[i].style.backgroundColor = "";
     }
 }
+
+function blackSquares () {
+    document.querySelectorAll(".div2").forEach(link => link.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = "black";
+}))
+    document.getElementById("black").style.backgroundColor = "black";
+    document.getElementById("black").style.color = "white";
+    document.getElementById("rainbows").style.backgroundColor = "";
+}
+
+function rainbow () {
+    document.querySelectorAll(".div2").forEach(link => link.addEventListener('mouseover', (event) => {
+        let color = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6)
+        event.target.style.backgroundColor = `${color}`
+}))
+    let buttonColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6)
+    document.getElementById("rainbows").style.backgroundColor = `${buttonColor}`;
+    document.getElementById("black").style.backgroundColor = "";
+    document.getElementById("black").style.color = "black";
+}
+
+
+    
 
 
